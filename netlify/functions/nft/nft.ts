a9251dd8ee977fa88b8c51191b6e6f893dbea3ce
EvanPiro <dev.evanpiro@gmail.com>
2023-05-17 11:06:11 -0400

    promote contract via argument injection

--- netlify/functions/nft/nft.ts
+++ netlify/functions/nft/nft.ts
@@ -4,7 +4,7 @@ import { pipe } from "fp-ts/function";
 import { task, taskEither as te } from "fp-ts";
 import { StatusCodes } from "http-status-codes";
 import { TaskEither } from "fp-ts/TaskEither";
-import { ethers } from "ethers";
+import { Contract, ethers } from "ethers";
 import abi from "../../../src/NFTPrinterABI";
 import axios from "axios";
 import * as dotenv from "dotenv";
@@ -18,6 +18,9 @@ const contractAddress = "0xa47d17727fDe3826d92A77BE2f83F6fb1d7254e8";
 const supportedNetworks = ["sepolia"];
 
 const ipfsGateway = "https://cloudflare-ipfs.com/ipfs/";
+
+const provider = new ethers.providers.JsonRpcProvider(sepoliaRpcUrl);
+const nftContract = new ethers.Contract(contractAddress, abi.abi, provider);
 
 const NFTQuery = t.partial({
   network: t.string,
@@ -52,51 +55,53 @@ const validateNetwork = (q: INFTQuery): TaskEither<IAppError, INFTQuery> =>
         body: "Network not supported",
       });
 
-const toNFTData = (q: INFTQuery): TaskEither<IAppError, INFTData> =>
-  te.tryCatch(
-    async () => {
-      const provider = new ethers.providers.JsonRpcProvider(sepoliaRpcUrl);
-      const contract = new ethers.Contract(contractAddress, abi.abi, provider);
-      const uri = await contract.tokenURI(q.tokenId);
-      const cid = uri.replace("ipfs://", "");
-      const metadataUri = ipfsGateway + cid;
-      const { data } = await axios.get(metadataUri);
-      return {
-        ...data,
-        ...q,
-      };
-    },
-    () => ({
-      statusCode: StatusCodes.BAD_GATEWAY,
-      body: "Data retrieve issues on our end.",
-    })
-  );
+const toNFTData =
+  (contract: Contract) =>
+  (q: INFTQuery): TaskEither<IAppError, INFTData> =>
+    te.tryCatch(
+      async () => {
+        const uri = await contract.tokenURI(q.tokenId);
+        const owner = await contract.ownerOf(q.tokenId);
+        const cid = uri.replace("ipfs://", "");
+        const metadataUri = ipfsGateway + cid;
+        const { data } = await axios.get(metadataUri);
+        return {
+          ...data,
+          owner,
+          ...q,
+        };
+      },
+      () => ({
+        statusCode: StatusCodes.BAD_GATEWAY,
+        body: "Data retrieval issues on our end.",
+      })
+    );
 
-const checkForPrice = (d: INFTData): TaskEither<IAppError, INFTData> =>
-  te.tryCatch(
-    async () => {
-      console.log("check for price began");
-      const provider = new ethers.providers.JsonRpcProvider(sepoliaRpcUrl);
-      const contract = new ethers.Contract(contractAddress, abi.abi, provider);
-      // Because contract will error on tokenId not found, allow for not found.
-      try {
-        const price = await contract.getListing(d.tokenId);
-        return {
-          ...d,
-          price,
-        };
-      } catch (e) {
-        return {
-          ...d,
-          price: null,
-        };
-      }
-    },
-    (e) => ({
-      statusCode: StatusCodes.BAD_GATEWAY,
-      body: "Data retrieve issues on our end.",
-    })
-  );
+const checkForPrice =
+  (contract: Contract) =>
+  (d: INFTData): TaskEither<IAppError, INFTData> =>
+    te.tryCatch(
+      async () => {
+        console.log("check for price began");
+        // Because contract will error on tokenId not found, allow for not found.
+        try {
+          const price = await contract.getListing(d.tokenId);
+          return {
+            ...d,
+            price,
+          };
+        } catch (e) {
+          return {
+            ...d,
+            price: null,
+          };
+        }
+      },
+      (e) => ({
+        statusCode: StatusCodes.BAD_GATEWAY,
+        body: "Data retrieval issues on our end.",
+      })
+    );
 
 // Basic query string NFT lookup
 // Example: http://localhost:8888/.netlify/functions/nft?network=sepolia&contractAddress=0x25b6364A5979e0e7C2ca3124d3b5d0A365EF1259&tokenId=0
@@ -104,8 +109,8 @@ const handler = async (event: HandlerEvent, context: HandlerContext) =>
   pipe(
     toQuery(event),
     te.chain(validateNetwork),
-    te.chain(toNFTData),
-    te.chain(checkForPrice),
+    te.chain(toNFTData(nftContract)),
+    te.chain(checkForPrice(nftContract)),
     te.fold(task.of, (body) =>
       task.of({
         statusCode: StatusCodes.OK,
